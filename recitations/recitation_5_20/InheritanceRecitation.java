a10713f878189d461323b08f64305352a2848f6c
Alexey Ayzin <alexey.ayzin@gmail.com>
2025-05-20 15:31:50 -0400

    Commit: Recitation

---
+++ recitations/recitation_5_20/InheritanceRecitation.java
@@ -0,0 +1,21 @@
+class Vehicle {
+}
+
+class Person {
+
+}
+
+class Car extends Vehicle {
+}
+
+class Truck extends Vehicle {
+}
+
+class EmergencyVehicle extends Vehicle {
+}
+
+public class InheritanceRecitation {
+  public static void main(String[] args) {
+
+  }
+}
